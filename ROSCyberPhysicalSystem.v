33112bdeb844463eb0aba23ceb75ccbf44f716d3
Abhishek Anand <abhishek@abhishek-new.cs.cornell.edu>
2014-10-01 22:38:52 -0400

    some work

--- ROSCyberPhysicalSystem.v
+++ ROSCyberPhysicalSystem.v
@@ -10,17 +10,44 @@ Record ROSCyberPhysSystem := {
   (* a proof that nodes have unique names and locations *)
 }.
 
-Record Event (cp : ROSCyberPhysSystem) := {    
-    time : Time;
-    recdMesg : Message
+
+Class DecEq (T : Type) :=
+{
+    eqdec : forall (a b :T), {a=b} + {a<>b}
 }.
 
+(** In any run, there will only be a finitely
+    many events. So the collection of events
+    in the entire system can be represented
+    as a list. [Event] is a type
+    representing all events in the system *)
 
-Record EventOrdering (cp : ROSCyberPhysSystem) :=
+Class EventType (T: Type) 
+      {cp : ROSCyberPhysSystem} 
+      {deq: DecEq T}
+    := {    
+    eTime : T -> Time;
+    eLoc : T ->  (NodeIndex cp);
+    eLocIndex : T -> nat;
+    recdMesg : T -> Message    
+}.
+
+Require Export Coq.Init.Wf.
+
+(** Both send and receives are messages *)
+
+
+Record SystemOrder (E :Type)  
+    {cp : ROSCyberPhysSystem} 
+    {et : @EventType E cp}
+:=
 {
-    events : (NodeIndex cp) -> list (Event cp);
-    causedBy : 
-    
+    causedBy : E -> E -> Prop
+    localCausal : forall (e1 e2 : E),
+        (eLoc e1) = (eLoc e2)
+        -> causedBy e1 e2
+        -> 
+    causalWf : 
 }.
 
 
