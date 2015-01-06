b408ef54bafdc5ff5b6e7e6ec582daa6b8a48458
Abhishek Anand <abhishek@abhishek-new.cs.cornell.edu>
2015-01-06 14:18:10 -0500

    submitted to Bob

--- icreate.v
+++ icreate.v
@@ -1,23 +1,35 @@
 Add LoadPath "../../../nuprl/coq".
 Require Export Coq.Program.Tactics.
 Require Export LibTactics.
+(** printing × $\times$ #×# *)
+(** printing :> $:$ #:># *)
 
 Require Export ROSCyberPhysicalSystem.
 Require Export String.
 (* Require Export CoRN.ode.SimpleIntegration. *)
 
+(*
 Definition dotProduct (v1 v2 : TContR × TContR) : TContR :=
   λ t, {fst v1} t [*] {fst v2} t  [+] {snd v1} t [*] {snd v2} t.
+*)
+
+Definition project2D  (vec :TContR × TContR) (angle : TContR) : TContR.
+Admitted.
+
+Definition isDerivativeOf2D  (f f' :TContR × TContR) : Type :=
+  isDerivativeOf (fst f) (fst f') 
+  × isDerivativeOf (fst f) (fst f').
+
+(** CatchFileBetweenTagsStartCreate *)
 
 Record iCreate : Type := {
-  (* x, y co-ordinates *)
-  position :> TContR × TContR;
-  (* orientation *)
-  theta : TContR;
-  linVel : TContR × TContR;
+  position :> TContR × TContR;          (* x, y co-ordinates *)
+  theta : TContR;                       (* orientation *)
+  transVel : TContR × TContR;             (* x, y velocity of center *)
   omega : TContR;
 
   derivRot : isDerivativeOf omega theta;
-  derivTrans : isDerivativeOf linVel 
+  derivTrans : isDerivativeOf2D transVel position
 }.
 
+(** CatchFileBetweenTagsEndCreate *)
