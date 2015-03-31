bef796cb2bae783e283798a019e9f8c226b4c502
Abhishek Anand (on VAIO laptop) <abhishek.anand.iitg@gmail.com>
2015-03-31 05:03:36 +0100

    finished JB's proof

--- IRMisc/ContField.v
+++ IRMisc/ContField.v
@@ -538,7 +538,7 @@ Definition Cintegral
     (f : IContR) : IR :=
   integral _ _ _ _ (IContRCont_I f (scs_prf _ _ lr)).
 
-Instance Cintegral_wd : Proper 
+Global Instance Cintegral_wd : Proper 
     ((@st_eq (Build_SubCSetoid (ProdCSetoid RInIntvl RInIntvl) (fun p => fst p [<=] snd p)))
       ==> (@st_eq IContR) 
       ==> (@st_eq IR)) (Cintegral).
@@ -570,7 +570,7 @@ Definition inBounds (ib : IntgBnds) (x : IR) : Prop :=
 Definition IContREqInIntvl (ib : IntgBnds) (f g : IContR) :=
 (∀ x : RInIntvl, inBounds ib x -> {f} x [=] {g} x).
 
-Instance Cintegral_wd2 (ib : IntgBnds) : 
+Global Instance Cintegral_wd2 (ib : IntgBnds) : 
     Proper 
       ((IContREqInIntvl ib) ==> (@st_eq IR)) 
       (Cintegral ib).
@@ -774,6 +774,18 @@ Lemma TContRExt : forall (f : IContR) a b,
   a [=] b -> {f} a [=] {f} b.
 Proof.
   intro f. apply  csf_fun_wd.
+Qed.
+
+
+Global Instance IContR_proper :
+  Proper ((@st_eq IContR)
+      ==> (@st_eq (RI_R))) (getF).
+Proof.
+  intros ? ? Heq. unfold getF.
+  destruct x, y.
+  simpl.
+  simpl in Heq.
+  exact Heq.
 Qed.
 
 Definition mkIntBnd {a b : RInIntvl} 
--- JB.v
+++ JB.v
@@ -8,7 +8,7 @@ Lemma DI0 : ∀ (F F' : Cart2D TContR) (LD : TContR) (te : QTime),
   isDerivativeOf (X F') (X F)
   -> isDerivativeOf (Y F') (Y F)
   -> isDerivativeOf LD ((X F)* (X F) + (Y F)* (Y F) - 1)
-  -> (getF ((X F)* (X F) + (Y F)* (Y F) - 1)) (mkQTime 0 I) = 0
+  -> (getF ((X F)* (X F) + (Y F)* (Y F) - 1)) 0 = 0
   -> (∀ (t : QTime), ((mkQTime 0 I) <= t <= te ) 
       -> {X F'} t = - {Y F} t 
           ∧ {Y F'} t =  {X F} t 
@@ -26,7 +26,7 @@ Qed.
 Lemma DI : ∀ (Inv LD : TContR) (te : QTime),
   isDerivativeOf LD Inv
   -> {Inv} (mkQTime 0 I) = 0
-  -> (∀ (t : QTime), ((mkQTime 0 I) <= t <= te) ->  {LD} t = 0)
+  -> (∀ (t : QTime), (0 <= t <= te) ->  {LD} t = 0)
   -> {Inv} te = 0.
 Proof.
   intros ? ? ? Hld Heq0 Ht.
@@ -35,31 +35,53 @@ Proof.
   simpl. eauto 1 with ROSCOQ.
 Qed.
 
-Add Ring IRisaRing: (CRing_Ring TContR).
-
-Lemma isDerivativeUnique:
-  ∀ (F F1' F2' : TContR),
-  isDerivativeOf F1' F
-  → isDerivativeOf F2' F
-  → F1' [=]  F2'.
-Admitted.
-
   
 Lemma DI' : ∀ (F F' : Cart2D TContR) (LD : TContR) (te : QTime),
   isDerivativeOf (X F') (X F)
   -> isDerivativeOf (Y F') (Y F)
-  -> isDerivativeOf LD ((X F) [*] (X F) [+] (Y F) [*] (Y F)  [+] - 1)
+  -> isDerivativeOf LD ((X F) * (X F) + (Y F) * (Y F) - 1)
   -> ∀ (t : QTime), 
-        ((mkQTime 0 I) <= t <= te) 
-        -> {X F'} t = - {Y F} t 
+        {X F'} t = - {Y F} t 
         -> {Y F'} t =  {X F} t
         -> {LD} t = 0.
 Proof.
-  intros? ? ? ?  H1d H2d H3d ? Htb H1eq H2eq.
+  intros? ? ? ?  H1d H2d H3d ? H1eq H2eq.
   eapply DerivativeNormSqr with (Y':=(Y F')) in H1d; eauto.
-  eapply TContRDerivativePlus with (F:= (X F[*]X F[+]Y F[*]Y F)) 
-        (G:=-1) (G':=0) in H1d; eauto.
-  eapply isDerivativeUnique in H1d.
-  simpl un H1d H
-  Focus 2. exact H3d.
+  clear H2d.
+  pose proof (TContRDerivativeConst (closel [0]) I (-1)) as Hconst.
+  pose proof (TContRDerivativePlus  H1d Hconst) as Hadd.
+  clear H1d Hconst.
+  eapply isIDerivativeOfWdl in Hadd;
+    [|apply cm_rht_unit_unfolded].
+  unfold canonical_names.negate, Negate_instance_IR in Hadd.
+  eapply isIDerivativeOfWdr in Hadd;
+    [ | rewrite <- FConstOppIn; reflexivity].
+  eapply isDerivativeUnique in Hadd; [| exact H3d].
+  clear H3d.
+  unfold canonical_names.equiv.
+  rewrite Hadd.
+  autorewrite with IContRApDown.
+  rewrite H1eq.
+  rewrite H2eq.
+  autounfold with IRMC.
+  ring.
+Qed.
 
+
+Lemma DIEx : ∀ (F F' : Cart2D TContR) (LD : TContR) (te : QTime),
+  isDerivativeOf (X F') (X F)
+  -> isDerivativeOf (Y F') (Y F)
+  -> isDerivativeOf LD ((X F)* (X F) + (Y F)* (Y F) - 1)
+  -> (getF ((X F)* (X F) + (Y F)* (Y F) - 1)) 0 = 0
+  -> (∀ (t : QTime), (0 <= t <= te ) 
+      -> {X F'} t = - {Y F} t 
+          ∧ {Y F'} t =  {X F} t)
+  -> (getF ((X F)* (X F) + (Y F)* (Y F) - 1)) te = 0.
+Proof.
+  intros ? ? ? ? H1d H2d Hld Heq0 Ht.
+  eapply DI; eauto.
+  intros ? Hb.
+  apply Ht in Hb. clear Ht.
+  destruct Hb as [Htl Htr].
+  eapply DI'; eauto.
+Qed.
