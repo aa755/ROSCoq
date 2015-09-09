45df3e060895dedae4f37da8fdd60020898edc12
Abhishek Anand (@ Ubuntu15 VM on VAIO laptop) <abhishek.anand.iitg@gmail.com>
2015-09-08 22:52:41 -0400

    exported the roshask Node Monad to Coq, using extraction directives.

---
+++ src/shim/Haskell/RoshaskNodeMonad.v
@@ -0,0 +1,35 @@
+Require Import String.
+Require Import RoshaskMsg.
+
+Extraction Language Haskell.
+
+Axiom Node : Type -> Type.
+Extract Constant Node "'a" => "Ros.Node 'a".
+
+Axiom nreturn : forall {a:Type}, a  -> Node a.
+Extract Constant nreturn => "Ros.ROSCoqUtil.nreturn".
+(* Extract Constant nreturn "'a" => "Ros.ROSCoqUtil.nreturn 'a". *)
+
+Definition example : Node nat := nreturn 0.
+Recursive Extraction example.
+
+Axiom nbind : forall {a b:Type},  Node a -> (a -> Node b) -> Node b.
+Extract Constant nbind => "Ros.ROSCoqUtil.nbind".
+
+Require Import MathClasses.interfaces.monads.
+
+Instance ReturnInstanceNodeHaskell : MonadReturn Node := (fun A => @nreturn A).
+(* TODO : change the order of params in Haskell too *)
+Instance BindInstanceNodeHaskell : MonadBind Node := (fun A B f x=> @nbind A B x f).
+
+(* Assuming that, for all (Equiv) instances of equality on Node A, Node A is a monad. 
+   Should this assumption be only for eq?
+*)
+Axiom AssumeMonadNodeHaskell: forall nodeEq, @Monad Node nodeEq _ _.
+
+Instance MonadInstanceNodeHaskell : forall nodeEq, @Monad Node nodeEq _ _ :=
+  AssumeMonadNodeHaskell.
+
+
+
+
