9135d602a6247f968f977437d3b3d3bd6854c2c4
Abhishek Anand <abhishek.anand.iitg@gmail.com>
2015-09-29 11:37:59 -0400

    Update installDependencies.sh
    for Coq 8.5

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,3 +1,5 @@
 rm -rf dependencies
 mkdir dependencies
-cd dependencies ; git clone https://github.com/c-corn/corn.git ; cd corn; git checkout d829dc968878aee1bcf944ac470ac03fc3b670a5 -b ROSCoq; git submodule update --init --recursive; scons -j2
+cd dependencies ; git clone https://github.com/aa755/corn.git ; cd corn; git checkout  VCoq8.5 -b roscoq; git submodule update --init --recursive; scons -j2
+# replace -j2 with -jN where N is the number of free cores on your machine. This is important because the last
+# step takes a long time (several hours on a single core), but it is extremely parallelizable.
