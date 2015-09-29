e1fc275b760e6e3e5f8f7a968e5f0f9f938ff8e1
Abhishek Anand <abhishek.anand.iitg@gmail.com>
2015-09-29 11:44:19 -0400

    Update installDependencies.sh

--- installDependencies.sh
+++ installDependencies.sh
@@ -1,5 +1,5 @@
 rm -rf dependencies
 mkdir dependencies
-cd dependencies ; git clone https://github.com/aa755/corn.git ; cd corn; git checkout  VCoq8.5 -b roscoq; git submodule update --init --recursive; scons -j2
+cd dependencies ; git clone https://github.com/aa755/corn.git ; cd corn; git checkout  VCoq8.5; git submodule update --init --recursive; scons -j2
 # replace -j2 with -jN where N is the number of free cores on your machine. This is important because the last
 # step takes a long time (several hours on a single core), but it is extremely parallelizable.
