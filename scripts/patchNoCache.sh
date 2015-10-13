3b06e1fc7f3193ed2d3d9960745a3c53e34cd8a5
Abhishek Anand (@ CU desktop) <abhishek.anand.iitg@gmail.com>
2015-10-13 18:22:24 -0400

    fixed coqdoc for examples/icreateMoveToLocExt.v

--- scripts/patchNoCache.sh
+++ scripts/patchNoCache.sh
@@ -1,6 +1,7 @@
+#designed to be run in ROSCoq/examples/coqdoc
 for f in *.html
 do 
-	cat ../doc/noCacheHeader.html > temp
+	cat ../../doc/noCacheHeader.html > temp
 	tail -n +5 $f >> temp
 	mv temp  $f
 done
