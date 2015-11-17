2f5b81dc9f37dae140148e85ea431903e40b5f77
Abhishek Anand (@ CU desktop) <abhishek.anand.iitg@gmail.com>
2015-11-17 14:07:19 -0500

    started writing the certified simulator, to obtain animations for the presentation

---
+++ src/robots/ackermannSteeringSimulator.v
@@ -0,0 +1,44 @@
+Require Export Coq.Program.Tactics.
+Require Export LibTactics.
+(** printing × $\times$ #×# *)
+(** printing :> $:$ #:># *)
+(** printing ≡ $\equiv$ #≡# *)
+(** printing [*] $*$ #*# *)
+(** printing ∧ $\wedge$ #∧# *)
+(** printing ∀ $\forall$ #∀# *)
+(** printing → $\rightarrow$ #→# *)
+(** printing ∃ $\exists$ #∃# *)
+(** printing ≤ $\le$ #≤# *)
+(** printing θ $\theta$ #θ# *)
+(** printing eeev $\epsilon_v$ #∊ᵥ# *)
+(** printing eeew $\epsilon_w$ #∊w# *)
+(** printing tm $t_m$ #tm# *)
+(** printing IR $\mathbb{R}$ #IR# *)
+(** printing CR $\mathbb{R}$ #CR# *)
+(** printing tr $t_r$ #tr# *)
+(** remove printing * *)
+
+(** printing ' $ $ #'# *)
+
+Require Import Vector.
+Require Import CPS.
+Require Import CPSUtils.
+
+Require Import MathClasses.interfaces.canonical_names.
+Require Import MCInstances.
+Require Import CartCR.
+Require Export ackermannSteering.
+Require Export ackermannSteeringProp.
+
+  Add Ring RisaRing: (CRing_Ring IR).
+
+Require Export CartIR.
+
+  
+Local Opaque CSine.
+Local Opaque CCos.
+Local Opaque Sine.
+Local Opaque Cosine.
+Local Opaque Sin.
+Local Opaque Cos.
+
