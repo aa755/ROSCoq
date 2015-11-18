5dcf7cd3653e7c26a5b21ddda84ab3a4bef3eafa
Abhishek Anand (CoRN VM @ vaio) <abhishek.anand.iitg@gmail.com>
2015-11-18 00:02:56 -0500

    defined the core of the atomic move simulator. need to prove it correct

--- src/robots/ackermannSteeringSimulator.v
+++ src/robots/ackermannSteeringSimulator.v
@@ -46,6 +46,48 @@ Local Opaque Cos.
 Record Rigid2DState : Type :=
 {
   pos2D : Cart2D CR;  
-  theta2D :  CR
+  θ2D :  CR
 }.
+
+(** enough data to render a car in a picture, which will be a part of an animation*)
+Record carState : Type :=
+{
+  csrigid2D : Rigid2DState;  
+  cs_tc :  CR (*turn curvatire, determines the position of steering wheel*)
+}.
+
+SearchAbout CRapartT.
+Open Scope CR_scope.
+
+
+(** with some effort, it should be possible to get rid of the 
+   second component of the dependent pair, because of continuity.*)
+Definition DAtomicMove := {am : AtomicMove 
+    | (am_tc am = 0 or (am_tc am) >< 0) }.
+
+Definition unitVecCR (θ : CR) : Cart2D CR := {| X := cos θ; Y := sin θ |}.
+Open Scope mc_scope.
+
+Close Scope CR_scope.
+
+Typeclasses  eauto := 100.
+
+Global Instance castCRCart2DCR : Cast CR (Cart2D CR) := sameXY.
+
+Definition stateAfterAtomicMove 
+  (cs : carState) (dm : DAtomicMove): carState :=
+  
+  let tc : CR := (am_tc (projT1 dm)) in
+  let dist : CR := (am_distance (projT1 dm)) in
+  let θInit :CR := (θ2D (csrigid2D cs)) in
+  let θf :CR := θInit + tc*dist in
+  let posInit : Cart2D CR := (pos2D (csrigid2D cs)) in
+  let posDelta := 
+    match (projT2 dm) with
+    | inl _ =>  ('dist) * (unitVecCR θInit)
+    | inr p => {|X:= (sin θf - sin θInit) * (CRinvT tc p) ; 
+                Y:= (cos θInit - cos θf) * (CRinvT tc p)|}
+    end in  
+  {|csrigid2D := {|pos2D := posInit + posDelta; θ2D := θf|} 
+    ; cs_tc :=tc |}.
 
