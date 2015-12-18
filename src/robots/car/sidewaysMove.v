aadd6ac9ae246d42020be9b92a2d487ff1340549
Abhishek Anand (on VAIO laptop) <abhishek.anand.iitg@gmail.com>
2015-12-18 01:50:20 +0000

    simplified the proofs in sidewaysMove.v using the lemma commutators.v The new versions of these lemas, which were only concerned with the final state after the move, will now be easier to use for space analysis during the move.

--- src/robots/car/sidewaysMove.v
+++ src/robots/car/sidewaysMove.v
@@ -35,6 +35,7 @@ Require Import MathClasses.orders.rings.
 Require Import MathClasses.interfaces.orders.
 Require Import ackermannSteeringProp.
 Require Import atomicMoves.
+Require Import IRMisc.LegacyIRRing.
 
   Local Notation minxy := (lstart).
   Local Notation maxxy := (lend).
@@ -49,8 +50,6 @@ Local Opaque Sin.
 Local Opaque Cos.
 
 Section Wriggle.
-Context {maxTurnCurvature : Qpos}
-(acs : AckermannCar maxTurnCurvature).
 (** * The Wriggle move
 Now consider a 
 #<href="https://rigtriv.wordpress.com/2007/10/01/parallel-parking/">wiggle motion</a>#
@@ -67,84 +66,69 @@ Informally it denotes the following motion :
   Wiggle is parametrized by a nonzero [turnCurvature] and a drive distance,
   both of which may be signed.
   *)
-  Variable tc : IR.
-  Hypothesis tcNZ : tc[#]0.
-  Variable distance : IR.
+Variable α : IR.
+Hypothesis tcNZ : α[#]0.
+Variable d : IR.
   
-
+Local Notation turnRadius (*: IR *) := (f_rcpcl α tcNZ).
 (** In our formalism, wriggle is a composition of the following 2 atomic moves.
   *)
   
-  Definition steerAndDrive : AtomicMove
-    := {|am_tc := tc; am_distance := distance |}.
-  Definition revSteerAndrevDrive : AtomicMove
-    := {|am_tc := -tc; am_distance := -distance |}.
-
-(** the distance covered during driving and reverse driving is exactly the same.
+Definition steerAndDrive : DAtomicMove:= 
+  existT _ 
+          {|am_tc := α; am_distance := d |} 
+          (inr tcNZ).
+  (*note that [revSteerAndrevDrive] is not the same as
+  - steerAndDrive*)
+Definition revSteerAndrevDrive : DAtomicMove :=
+   existT _ 
+     {|am_tc := -α; am_distance := -d |}
+      (inr (tcnegNZ _ tcNZ)).
+(** the distance covered during driving and 
+  reverse driving is exactly the same.
   TODO: let them be slightly different, e.g. upto epsilon
  *)
-  Definition Wriggle : AtomicMoves 
-    :=  [steerAndDrive; revSteerAndrevDrive].
-  
-  Variable tstart : Time.
-  Variable tend : Time.
-  Hypothesis timeInc : tstart ≤ tend.
-  
-  (** Now, we assume that the car executes the [Wriggle] move from time [tstart] to [tend],
-    and characterize the position and orientation at [tend], in terms of their values at [tstart]. 
-     In this document When we say "move" in natural language, we mean [AtomicMoves].
-    *)
-  Hypothesis amsc : CarExecutesAtomicMovesDuring acs Wriggle tstart tend 
-                      (timeInc).
-  
-  
-  Local Notation θs := ({theta acs} tstart).
-  Local Notation Xs := ({X (position acs)} tstart).
-  Local Notation Ys := ({Y (position acs)} tstart).
+Definition Wriggle : list DAtomicMove :=  
+  [steerAndDrive; revSteerAndrevDrive].
 
-  
-  Hint Unfold One_instance_IR : IRMC.
-      
-      
-  
-  Lemma Wriggleθ : {theta acs} tend =  θs + 2 * tc * distance.
-  Proof using amsc.
-    invertAtomicMoves. rename Hf into amscrl.
-    apply AtomicMoveθ in amscl.
-    apply AtomicMoveθ in amscrl.
-    simpl in amscl, amscrl. rewrite amscl in amscrl.
-    rewrite amscrl.
-    autounfold with IRMC. ring.    
-  Qed.
+(*
+Add Ring tempRingIR : (stdlib_ring_theory IR).
+ *)
 
-  (** just to show that the car doesn't end up to the initial position after wriggle.
-     This equation is not needed for anything else. *)
-  Lemma WriggleX : {X (position acs)} tend =  Xs +
-        ((2* Sin (θs + tc * distance) 
-            - Sin (θs + 2 * tc * distance)  - Sin θs) [/] tc [//] tcNZ).
-  Proof using All.
-    pose proof Wriggleθ as XX.
-    invertAtomicMoves.
-    rename amscl into Hl.
-    rename Hf into Hrr.
-    pose proof Hl as Hlt.
-    apply AtomicMoveθ in Hlt.
-    apply AtomicMoveX with (tcNZ0:= tcNZ) in Hl.
-    apply AtomicMoveXT with (tcNZ0:= tcnegNZ _ tcNZ) in Hrr.
-    Local Opaque Cos Sin.
-    simpl in Hl, Hrr, Hlt.
-    unfold cf_div in Hrr.
-    rewrite XX in Hrr.
-    rewrite Hrr. rewrite Hl.
-    autounfold with IRMC. unfold cf_div.
-    rewrite reciprocalNeg with (xp:=tcNZ).
-    rewrite cring_inv_mult_lft.
-    rewrite <- cring_inv_mult_rht.
-    setoid_rewrite minusInvR.
-    rewrite Hlt.
-    autounfold with IRMC. unfold cg_minus. simpl.
-     ring.
-  Qed.
+Definition unitVecT `{SinClass R} `{CosClass R} (t:R) := transpose (unitVec t).
+
+Hint Unfold One_instance_IR : IRMC.
+
+(** i, m, f respectively stand for initial, 
+  middle, final
+ *)
+
+Lemma WriggleState : forall init,
+  let θi := θ2D init in
+  let θm := θi +  α * d in
+  let θf := θi + 2 * α * d in
+  let pf := pos2D init +
+    (2*(unitVecT θm) - (unitVecT  θf) - (unitVecT θi))
+    *{|X:=1;Y:=-1|}*'turnRadius in
+  stateAfterAtomicMoves Wriggle init
+  = {|pos2D := pf ; θ2D:= θf|}.
+Proof using.
+  intros ? ? ? ?. simpl.
+  unfold stateAfterAtomicMove. simpl.
+  split; simpl; [| subst  θi θm θf; simpl;
+  IRring].
+  rewrite reciprocalNeg with (xp:=tcNZ).
+  fold Negate_instance_IR.
+  fold (@negate IR _).
+  assert 
+    (θ2D init + α * d + - α * - d
+    = θ2D init + 2 * α * d) as H by IRring.
+  rewrite H.
+  fold θi.
+  fold θm.
+  fold θf.
+  split; simpl; try IRring.
+Qed.
 
 End Wriggle.
 
@@ -164,8 +148,6 @@ End Wriggle.
 
 Section Sideways.
 
-Context {maxTurnCurvature : Qpos}
-(acs : AckermannCar maxTurnCurvature).
 
 (** * The sideways move
 
@@ -179,62 +161,36 @@ a wriggle and its inverse.
 Note that without this insertion, we would have been back
 to where we started.
 *)
-  Variable tc : IR.
-  Hypothesis tcNZ : tc[#]0.
-  Variable wdistance : IR.
-  Variable ddistance : IR.
+  Variable α : IR.
+  Hypothesis tcNZ : α[#]0.
+  Variable d : IR.
+  Variable d' : IR.
   
-  Local Notation SWriggle := (Wriggle tc wdistance).
-  Local Notation SWriggleInv := (AtomicMovesInv SWriggle).
+  Local Notation SWriggle := (Wriggle α tcNZ d).
   
+  Require Import commutator.
   (** Drive a distance of [ddistance]
     with front wheels perfectly straight.*)  
-  Local Notation DriveStraight := {| am_distance := ddistance ; am_tc := 0|}.
-
-  Definition SidewaysAux : AtomicMoves 
-    := SWriggle ++ [DriveStraight] ++ SWriggleInv.
+  Definition SidewaysAux : list DAtomicMove 
+    := conjugate [mkStraightMove d'] SWriggle.
 
   
   (** The car's orientation at the end is same as that at the start.
      [θAtW] denotes the car's orientation at the completion of the [SWriggle] move. 
      For any [v], [sameXY v] denotes [{|X:=v; Y:=v|}].
      *)
-  Lemma SidewaysAuxState : forall  (tstart tend : Time) (timeInc : tstart ≤ tend),
-  (CarExecutesAtomicMovesDuring acs SidewaysAux tstart tend timeInc)
-  ->
-  let θs := ({theta acs} tstart) in 
-  let θAtW := θs + 2 * tc * wdistance  in
-  {theta acs} tend =  θs /\
-    posAtTime acs tend = (posAtTime acs tstart)
-      + ('ddistance) * (unitVec θAtW).
-  Proof using Type.
-    intros ? ? ? amsc.    
-    unfold SidewaysAux in amsc.
-    apply movesControlsApp in amsc.
-    destruct amsc as [tds Hams]. (* ds for drive straight *)
-    destruct Hams as [pds Hams].
-    repnd. rename Hamsl into Hw. (* w for wiggle *)
-    apply movesControlsApp in Hamsr.
-    destruct Hamsr as [twr Hamsr]. (* ds for drive straight *)
-    destruct Hamsr as [pwr Hams]. repnd.
-    rename Hamsl into Hds.
-    rename Hamsr into Hwr.
-    pose proof Hw as Hwb. (* needed for θAtW *)
-    eapply atomicMovesInvertible in Hw; eauto.
-    specialize (Hw Hwr). clear Hwr.
-    apply Wriggleθ in Hwb.
-    invertAtomicMoves.
-    apply AtomicMoveZFinal in Hf;[|unfold amNoTurn;  reflexivity]. destruct Hf as [Hfr Hfl].
-    simpl in Hfl, Hfr.
-    specialize (Hw Hfl).
-    repnd. symmetry in Hwr.
-    split;[assumption|]. rewrite Hwb in Hfr.
-    clear Hwb Hwr Hfl pll. rewrite Hfr in Hwl. clear Hfr.
-    rewrite  <- (@plus_assoc (Cart2D IR)) in Hwl; [|eauto with typeclass_instances].
-    apply (@left_cancellation ) in Hwl; [|apply groups.LeftCancellation_instance_0].
-    apply (RingNegateProper ) in Hwl.
-    rewrite negate_involutive in Hwl.
-    rewrite Hwl. ring.
+  Lemma SidewaysAuxState : forall init,
+  stateAfterAtomicMoves SidewaysAux init
+  = {|pos2D := pos2D init 
+      + '(d') * unitVec ((θ2D init) + 2 * α * d);
+      θ2D := θ2D init |}.
+  Proof using.
+    intros.
+    unfold SidewaysAux.
+    rewrite straightConjugateState.
+    rewrite WriggleState.
+    simpl.
+    reflexivity.
   Qed.
 
   (** After [SidewaysAux], the car is in the same orientation as before, but it has position
@@ -246,9 +202,9 @@ to where we started.
     orthogonal to its orientation.
     *)
   Local Notation DriveStraightRev 
-    := {| am_distance := - ddistance * Cos (2 * tc * wdistance) ; am_tc := 0|}.
+    := (mkStraightMove (- d' * cos (2 * α * d))).
 
-  Definition SidewaysMove : AtomicMoves 
+  Definition SidewaysMove : list DAtomicMove 
     := SidewaysAux ++ [DriveStraightRev].
     
   (** The car's final orientation is same as before, and 
@@ -258,46 +214,54 @@ to where we started.
 
   As mentioned before, for any [v], [sameXY v] denotes [{|X:=v; Y:=v|}].
   *)
-  
-  Lemma SidewaysState : forall  (tstart tend : Time) (timeInc : tstart ≤ tend),
-  (CarExecutesAtomicMovesDuring acs SidewaysMove tstart tend timeInc)
-  ->
-  let θs := ({theta acs} tstart) in 
-  let θw := 2 * tc * wdistance  in
-    {theta acs} tend =  θs /\
-    posAtTime acs tend = (posAtTime acs tstart) 
-      + ('(ddistance * Sin  θw)) * unitVec (θs + (½ * π)).
-  Proof using Type.
-    intros ? ? ? amsc.
-    unfold SidewaysMove in amsc. simpl.
-    apply movesControlsApp in amsc.
-    destruct amsc as [tds Hams]. (* ds for drive straight *)
-    destruct Hams as [pds Hams]. repnd.
-    apply SidewaysAuxState in Hamsl.
-    invertAtomicMoves.
-    apply AtomicMoveZFinal in Hf;[|unfold amNoTurn;  reflexivity].
-     destruct Hf as [Hfr Hfl].
-    simpl in Hfl, Hfr. repnd.
-    rewrite Hamsll in Hfl.
-    rewrite Hamsll in Hfr.
-    split;[assumption|]. clear Hamsll Hfl.
-    rewrite Hamslr in Hfr. clear Hamslr pll pdsl pdsr.
-    remember (2 * tc * wdistance) as θw.
-    clear Heqθw. unfold cast, castCRCart2DCR in Hfr.
-    rewrite <- sameXYMult in Hfr.
-    rewrite sameXYNegate in Hfr.
-    rewrite  <- (@mult_assoc (Cart2D IR)) in Hfr ; [|eauto with typeclass_instances].
-    
-    rewrite <- negate_mult_distr_l in Hfr.
-    rewrite negate_mult_distr_r in Hfr.
-    rewrite  <- (@plus_assoc (Cart2D IR)) in Hfr; [|eauto with typeclass_instances].
-    setoid_rewrite  <- (@plus_mult_distr_l (Cart2D IR)) in Hfr;
-       [|eauto with typeclass_instances].
-    rewrite unitVecLemma1 in Hfr. unfold cast, castCRCart2DCR.
-    rewrite <- sameXYMult.
-    rewrite  <- (@mult_assoc (Cart2D IR)); [|eauto with typeclass_instances].
-    rewrite PiBy2DesugarIR.
-    exact Hfr.
-  Qed.
 
+(* maybe delete from CartIR *)
+Lemma unitVecLemma1 : forall θs θw:IR, 
+  (unitVec (θs + θw)  - '(cos θw) * unitVec θs)
+  = ('(sin  θw)) * unitVec (θs + (½ * π)).
+Proof.
+  intros ? ?.
+  unfold cast,castCRCart2DCR,
+     sameXY, unitVec.   autounfold with IRMC.
+  rewrite PiBy2DesugarIR.  
+  rewrite Sin_plus_HalfPi.
+  rewrite Cos_plus_HalfPi.
+  simpl. split; simpl; autounfold with IRMC;
+  [rewrite Cos_plus | rewrite Sin_plus]; try IRring.
+Qed.
+
+    Local Opaque SidewaysAux.
+  Lemma SidewaysState : forall init,
+  let θw := 2 * α * d  in
+  stateAfterAtomicMoves SidewaysMove init
+  = {|pos2D := pos2D init 
+      + ('(d' * sin  θw)) * unitVec (θ2D init + (½ * π));
+      θ2D := θ2D init |}.
+  Proof using Type.
+    intros ?.
+    simpl.
+    unfold SidewaysMove, mkStraightMove,
+      stateAfterAtomicMoves.
+    rewrite fold_left_app.
+    simpl.
+    fold stateAfterAtomicMoves.
+    rewrite SidewaysAuxState.
+    unfold stateAfterAtomicMove.
+    simpl. rewrite mult_0_l, plus_0_r.
+    split;[| reflexivity].
+    simpl. 
+    rewrite <- (@simple_associativity  _ _ plus _ _).
+    fequiv.
+    remember (2 * α * d) as θw. clear Heqθw.
+    rewrite <- negate_mult_distr_l.
+    rewrite  preserves_negate.
+    rewrite  preserves_mult.
+    rewrite negate_mult_distr_r.
+    rewrite <- (@simple_associativity _ _ mult _ _ _).
+    rewrite <- (@simple_distribute_l _ _  mult plus _ _ _ _).
+    rewrite <- negate_mult_distr_l.
+    rewrite unitVecLemma1.
+    rewrite  preserves_mult.
+     ring.
+  Qed.
 End Sideways.
